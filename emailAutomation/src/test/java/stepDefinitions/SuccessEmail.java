050e1722c2ad7703779670b5c0f54c4d95e0634c
fredr <fredr@DESKTOP-PJCOQA0>
2021-04-04 19:16:42 +0200

    Updated feature file and generalized stepDef + random email and name.

--- emailAutomation/src/test/java/stepDefinitions/SuccessEmail.java
+++ emailAutomation/src/test/java/stepDefinitions/SuccessEmail.java
@@ -1,5 +1,8 @@
 package stepDefinitions;
 
+import java.util.Random;
+
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,42 +14,54 @@ import io.cucumber.java.en.When;
 
 public class SuccessEmail {
 
-	public WebDriver driver;
+	private WebDriver driver;
+	Random generate = new Random();
 	
 	@Given("User is on Sign up page")
 	public void user_is_on_sign_up_page() {
-	   System.setProperty("webdriver.chrome.driver", "C:\\Webbdrivers\\chromedriver.exe");
-	   
+	   System.setProperty("webdriver.chrome.driver", "C:\\Webbdrivers\\chromedriver.exe"); 
 	   driver = new ChromeDriver();
 	   driver.manage().window().maximize();
 	   driver.get("https://login.mailchimp.com/signup/");
 	}
 
-	@When("user enters email1")
-	public void user_enters_testing1() {
-		 WebElement email = driver.findElement(By.id("email"));
-		 email.sendKeys("emailaddress@123.com");
+	@When("user enters {string}")
+	public void user_enters_email(String emailAddress) {
+		
+		WebElement mail = driver.findElement(By.id("email"));
+		int emailNumber = generate.nextInt();
+		mail.sendKeys(emailNumber + 1000 + "@abc.com"); 					//The e-mail webElement required a minimum of 4 numbers and a @ to be supported.
+		
+	}
+	@When("user selects a {string}")
+	public void user_selects_a_username(String username) {
+		
+		int userNumber = generate.nextInt(); 
+		WebElement name = driver.findElement(By.id("new_username"));
+		name.sendKeys(userNumber + "");										// Successful random name
+		//name.sendKeys("FredrikLundmark");									// Taken userName
 	}
 	
-	@When("user selects a username1")
-	public void user_selects_a_username1() {
-		 WebElement username = driver.findElement(By.id("new_username"));
-		 username.sendKeys("Fredrik");
+	@When("user types a {string}")
+	public void user_types_a_password(String password) {
+		 WebElement pass = driver.findElement(By.id("new_password"));
+		 pass.sendKeys("Abcdef1!");
 	}
-	@When("user types a password1")
-	public void user_types_a_password1() {
-		 WebElement password = driver.findElement(By.id("new_password"));
-		 password.sendKeys("Abcdef1!");
-	}
+	
 	@When("user click on sign up button")
 	public void user_click_on_sign_up_button() {
-		// System.out.println("");
 		 WebElement button = driver.findElement(By.id("create-account"));
-		 button.click();
+		// button.click();
 	}
-	@Then("the result will be shown")
-	public void the_result_will_be_shown() {
-		 System.out.println("h1");
+	
+	@Then("the {string} will be shown")
+	public void the_result_will_be_shown(String result) {
+		//String actualError = driver.findElement(By.className("invalid-error")).getText();								
+		//String expectedError = "Another user with this username already exists. Maybe it's your evil twin. Spooky.";		//Taken username test
+		//Assert.assertEquals(actualError, expectedError);
+		
+		//driver.close();
+				 
 	}
 
 }
--- emailAutomation/src/test/resources/Features/email.feature
+++ emailAutomation/src/test/resources/Features/email.feature
@@ -4,16 +4,15 @@ Feature: Email Registration
     Given User is on Sign up page
 
   Scenario Outline: Sign up registration outline
-    When user enters <EmailAddress>
-    And user selects a <Username>
-    And user types a <Password>
+    When user enters "<EmailAddress>"
+    And user selects a "<Username>"
+    And user types a "<Password>"
     And user click on sign up button
-    Then the result will be shown
+    Then the "<Result>" will be shown
 
     Examples: 
-      | EmailAddress | Username  | Password  |
-      | email1       | username1 | password1 |
-      | email2       | username2 | password2 |
-      | email3       | username3 | password3 |
-      | email4       | username4 | password4 |
-
+      | EmailAddress  | Username        | Password | Result      |
+      | successEmail  | successUsername | password | success     |
+      | email2        | LongUsername    | password | failLong    |
+      | email3        | TakenUsername   | password | failTaken   |
+      | Missing email | username        | password | failMissing |
